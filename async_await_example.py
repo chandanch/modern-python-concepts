8ba17c0a1c5b280911ff5619eae6f3d0d4a12d0e
chandanch <chandan.cec107@gmail.com>
2023-01-26 10:26:48 +0530

    async await with task example

---
+++ async_await_example.py
@@ -0,0 +1,28 @@
+import asyncio
+
+
+async def main_async_await():
+    """
+        Using Async Await keyword
+    """
+    print("Start of Async Await Explore!!")
+    await proccess_data("My Async func")
+    print("Data Processing Completed..")
+
+
+async def main_async_task():
+    """
+        Using Async with task for non-blocking or parallel execution
+    """
+    print("Start of Async Await Explore!!")
+    task = asyncio.create_task(proccess_data("My Async func"))
+    await task
+    print("Data Processing Completed..")
+
+
+async def proccess_data(data):
+    print(f"Processing Data: {data}")
+    await asyncio.sleep(5)
+    print("Finializing Data")
+
+asyncio.run(main_async_task())
