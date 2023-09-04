9384bf630c099f19357072e60512dff4076b09e8
Chandan CH <chandanch@users.noreply.github.com>
2023-09-04 20:58:36 +0530

    Update async_await_example2.py

--- async_await_example2.py
+++ async_await_example2.py
@@ -7,6 +7,7 @@ async def fetch_products():
     return {"name": "Jim Can Supplies", "code": 12922, "price": 13.23}
 
 async def main():
+    # create task
     task1 = asyncio.create_task(fetch_products())
     product = await task1
     print(product)
